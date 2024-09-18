5f9a2dc097e17f4acb92300887b5765202df6648
ebr <clkebr2601@gmail.com>
2024-09-18 16:04:52 +0200

    infrastructure is ready

---
+++ src/main/java/com/zyfera/ZyferaApplication.java
@@ -0,0 +1,13 @@
+package com.zyfera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyferaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyferaApplication.class, args);
+	}
+
+}
